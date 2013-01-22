3a8e1a4dc923cbe8ce0d818572313a868b37f740
Donny Winston <dwinston@alum.mit.edu>
2013-01-22 15:08:17 -0800

    Order items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.order(:name)
 
     respond_to do |format|
       format.html # index.html.erb
